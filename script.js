c25e21d3c89475c614fee6ee759f989ea9fdda05
RstK <208619198+bowman025@users.noreply.github.com>
2025-07-08 16:15:26 +0200

    Create variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+let userInput = null;
+let userNumber = null;
+let userOperator = null;
+let operandA = null;
+let operandB = null;
+let result = null;
+
